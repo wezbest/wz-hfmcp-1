5a2725e2506fd16e9d2d84d0a6fcdfd5f73a1962
wezbest <110793713+wezbest@users.noreply.github.com>
2025-05-31 00:22:23 +0000

    🫦

--- WX/c1/src/c4.py
+++ WX/c1/src/c4.py
@@ -13,6 +13,19 @@ from .utz import header1
 def chat_c4():
     header1("Streaming Chatbot Tests")
 
+    # Intro Message
+    intro_message = """
+# Streaming Chatbot Example
+1. Testing making blocks and stream 
+"""
+
+    # Defining the main block
+    with gr.Blocks() as demo:
+
+        # Defining the intro block
+        with gr.Tab("Intro"):
+            gr.Markdown(intro_message)
+
     def slow_echo(message, history):
         for i in range(len(message)):
             time.sleep(0.05)
