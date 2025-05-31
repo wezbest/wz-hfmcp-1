425ad99360ec0e9dbf04e029cf0d59a561c77b3e
wezbest <110793713+wezbest@users.noreply.github.com>
2025-05-31 12:36:37 +0000

    🫦

--- WX/c2/app.py
+++ WX/c2/app.py
@@ -1,5 +1,10 @@
+# Main entry point for app
+
+from src.c1 import c1_main_func
+
+
 def main():
-    print("Hello from c2!")
+    c1_main_func()
 
 
 if __name__ == "__main__":
---
+++ WX/c2/src/c1.py
@@ -0,0 +1,16 @@
+# //////////////////////////////////////////////////
+# Testing basic input and output in gradio
+# //////////////////////////////////////////////////
+
+from .utz import header1
+import gradio as gr
+
+
+def c1_main_func():
+    c1()
+
+
+def c1():
+    header1("Chat 1")
+
+    
