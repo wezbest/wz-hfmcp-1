7bfd428234ff20e6cf280953a1a7a410d7d5396f
wezbest <110793713+wezbest@users.noreply.github.com>
2025-05-31 18:24:24 +0000

    🫦

--- WX/c2/src/ch1.py
+++ WX/c2/src/ch1.py
@@ -2,13 +2,20 @@
 # Testing of chatbot behavior in this
 # //////////////////////////////////////////////////
 
+from .utz import header1
 import gradio as gr
 import sambanova_gradio as sg
+from dotenv import load_dotenv
+impor os
 
-from .utz import header1
 
+# -- Get tokens ---
+load_dotenv("./src/.env")
+hf_token = os.getenv("HF")
 
 # --- Main Function caller ---
+
+
 def ch1_mf():
     chatbot1_sambanova()
 
@@ -19,5 +26,9 @@ def chatbot1_sambanova():
 
     header1("Chatbot 1 - Llama-4-Maverick-17B-128E-Instruct")
 
-    gr.load("Llama-4-Maverick-17B-128E-Instruct", src=sg.registry,
-            accept_token=True, multi_model=True).launch()
+    gr.load("Llama-4-Maverick-17B-128E-Instruct",
+            src=sg.registry,
+            accept_token=True,
+            multi_model=True,
+            token="",
+            ).launch()
