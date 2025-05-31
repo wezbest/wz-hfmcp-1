8549344dfe032061b1d38fc78a2f11008ccbb763
wezbest <110793713+wezbest@users.noreply.github.com>
2025-05-31 12:56:55 +0000

    🫦

--- WX/c2/README.md
+++ WX/c2/README.md
@@ -6,3 +6,7 @@
 > This project is for testing various layout methods and related. Files will be stored in [`src`](./src/)
 
 # SrcFilezDescription
+
+|          File          |               Desc                |
+| :--------------------: | :-------------------------------: |
+| [`c1.py`](./src/c1.py) | Basic layuot testing side by side |
---
+++ WX/c2/src/c2.py
@@ -0,0 +1,37 @@
+# //////////////////////////////////////////////////
+# Side by side with tabs
+# //////////////////////////////////////////////////
+
+import gradio as gr
+
+from .utz import header1
+
+
+def c1_main_func():
+    c1()
+
+
+def c1():
+    header1("Chat 1")
+
+    def greet(name, emoji):
+        return f"Hello {name} {emoji}"
+
+    with gr.Blocks() as demo:
+
+        gr.Markdown("## Chat 1")
+        gr.Markdown("This is a simple greeting app using Gradio.")
+
+        with gr.Row():
+
+            with gr.Column():
+
+                name = gr.Textbox(label="Name", placeholder="Enter your name")
+                emoji = gr.Textbox(label="Emoji", placeholder="Enter an emoji")
+                greet_button = gr.Button("Greet")
+
+            with gr.Column():
+                output = gr.Textbox(label="Output")
+                greet_button.click(greet, inputs=[name, emoji], outputs=output)
+
+    demo.launch()
