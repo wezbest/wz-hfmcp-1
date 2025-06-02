c42a5530130b90773dee9c4d5aff4bad3436d205
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-02 18:21:06 +0000

    🫦

---
+++ WX/curs/c1/src/hft.py
@@ -0,0 +1,9 @@
+# /////////////////////////////////
+# HuggingFace API Tests
+# /////////////////////////////////
+
+from dotenv import load_dotenv
+from huggingface_hub import InferenceClient
+from rich import print as rprint  # For rprinting
+
+# -- Main entry Function ---
