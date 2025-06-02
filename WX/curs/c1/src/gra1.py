56c60feb3e522951288f8c54df3b03abce4b655d
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-02 16:39:29 +0000

    🫦

--- WX/curs/c1/src/gra1.py
+++ WX/curs/c1/src/gra1.py
@@ -1,3 +1,11 @@
 # /////////////////////////////////////
 # Gradio Interface tests
 # /////////////////////////////////////
+
+from src.utz import header1
+
+# --- Main chat function ---
+
+
+def gra1_main():
+    header1("Main chat function")
