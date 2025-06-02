857a75ccb7489975f8ade68dbe0a750ceb490ec3
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-02 16:49:38 +0000

    🫦

--- WX/curs/c1/src/gra1.py
+++ WX/curs/c1/src/gra1.py
@@ -4,10 +4,26 @@
 
 # --- Imports ---
 
+import gradio as gr
+
 from src.utz import header1
 
 # --- Main chat function ---
 
 
 def gra1_main():
-    header1("Main chat function")
+    gra1_chat1()
+
+
+# -- Sub functions call ---
+
+# Examples from the official documentation
+def gra1_chat1():
+    header1("Chat Interface One")
+
+    def echo(message, history):
+        return message
+
+    demo = gr.ChatInterface(fn=echo, type="messages", examples=[
+                            "hello", "hola", "merhaba"], title="Echo Bot")
+    demo.launch()
