7fde290ea951709a3899507f04be4272143ce7a7
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-03 17:25:30 +0000

    🫦

--- WX/curs/c1/src/grt1.py
+++ WX/curs/c1/src/grt1.py
@@ -3,9 +3,15 @@
 # //////////////////////////////////////////////////////////
 
 from src.utz import header1
+import gradio as gr
 
 # --- Main Function ---
 
 
 def grt1_main():
     header1("Gradio General Tests ")
+
+# --- Sub functions ----
+
+
+def grt1_f1():
