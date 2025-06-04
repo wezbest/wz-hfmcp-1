1b902019d76bcfbae1c1c77f95e60419ba0eaa19
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-04 15:32:23 +0000

    🫦

--- WX/curs/c1/src/grt1.py
+++ WX/curs/c1/src/grt1.py
@@ -2,10 +2,12 @@
 # GEneral Gradio tests in this section
 # //////////////////////////////////////////////////////////
 
-from src.utz import header1
-import gradio as gr
 import os
+
+import gradio as gr
 from dotenv import load_dotenv
+
+from src.utz import header1
 
 # --- Getting the ENV Variables ---
 
@@ -16,9 +18,27 @@ hf_token = os.getenv("HF")
 
 
 def grt1_main():
-    header1("Gradio General Tests ")
+    grt1_f1()
 
 # --- Sub functions ----
 
+# Trying to understand basic input and ouput here
+
 
 def grt1_f1():
+    header1("Gradio General Tests ")
+
+    def greet(name):
+        return name + "SmellFarts"
+
+    def tab1():
+        with gr.Tab("Smell Panty"):
+            gr.Image("../../../wo/085.jpg")
+            gr.Markdown("# Woman Si ltink Sniff")
+
+    with gr.Blocks() as demo:
+        tab1()
+        tab1()
+        tab1()
+
+    demo.launch()
