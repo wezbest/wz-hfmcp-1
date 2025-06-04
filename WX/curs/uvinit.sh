60bf80149701ff3561e5f88184048c81f48917b7
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-04 21:00:53 +0000

    🫦

--- WX/curs/uvinit.sh
+++ WX/curs/uvinit.sh
@@ -36,7 +36,7 @@ uv_gr() {
     CO1="uv init $name_of_project"
     CO2="cd $name_of_project"
 
-    DEPS="rich gradio[mcp] smolagents[all] DuckDuckGoSearchTool"
+    DEPS="rich gradio[mcp] smolagents tavily-python python-dotenv"
     CO3="uv add  $DEPS"
     CO4="uv tree"
 
