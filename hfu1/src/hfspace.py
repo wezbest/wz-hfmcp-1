38ac838feda498d29ab62e8cf679187111384d23
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-05 02:22:37 +0000

    🫦

--- hfu1/src/hfspace.py
+++ hfu1/src/hfspace.py
@@ -15,7 +15,7 @@ load_dotenv("src/.env")
 hf_token = os.getenv("HF")
 
 # Main Repo ID here
-REPO_ID = "Wornu/grh1"
+REPO_ID = "Wornu/pr1"
 
 
 #  ███████╗ ██╗   ██╗ ███╗   ██╗  ██████╗
@@ -30,7 +30,7 @@ def hf_space_ops():
     """
     Main function for Hugging Face repository operations.
     """
-    hf_delete_spaces()
+    # hf_delete_spaces()
     hf_create_space()
     hf_upload_dirz()
 
