600db77f35116a6ad333953340188ed104e4c351
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-05 02:31:20 +0000

    🫦

--- WX/mywo/pri1/UL/app.py
+++ WX/mywo/pri1/UL/app.py
@@ -4,8 +4,6 @@
 
 import gradio as gr
 
-from funcz import intro_tab, name_func
-
 # Theming variables
 
 themes = [
@@ -17,6 +15,60 @@ themes = [
     gr.themes.Soft()
 ]
 choose_theme = themes[5]
+
+# --- Functions --- 
+def name_func():
+
+    intro_text = """
+# Name Function
+1. This is the name function
+2. This gradio app is hidden and private
+3. You are writting this app so that it can be private and you can later on release if needed
+"""
+
+    gr.Markdown(intro_text)
+    inp = gr.Textbox(placeholder="What is your name Bastard Raper?")
+    out = gr.Textbox()
+
+    inp.change(fn=lambda x: f"Welcome, {x}!",
+               inputs=inp,
+               outputs=out)
+
+    examples = gr.Examples(
+        examples=[
+            ["Alice"],
+            ["Bob"],
+            ["Charlie"],
+        ],
+        inputs=[inp],
+    )
+
+# INtro tab
+
+
+def intro_tab():
+
+    description = """
+# Introduction Text
+1. This is the intro text in this tab
+2. This gradio app is hidden and private
+3. You are writting this app so that it can be private and you can later on release if needed
+
+# Second Section
+1. Writing a section here
+
+# Simple Table
+
+| Column 1 | Column 2 |
+|---------- | ----------|
+| Cell 1 | Cell 2 |
+| Cell 3 | Cell 4 |
+
+"""
+
+    gr.Markdown(description)
+
+
 
 # This is the actual name function
 
--- hfu1/src/hfspace.py
+++ hfu1/src/hfspace.py
@@ -30,7 +30,7 @@ def hf_space_ops():
     """
     Main function for Hugging Face repository operations.
     """
-    # hf_delete_spaces()
+    hf_delete_spaces()
     hf_create_space()
     hf_upload_dirz()
 
@@ -68,6 +68,7 @@ def hf_create_space():
         repo_id=REPO_ID,
         repo_type="space",
         space_sdk="gradio",
+        private="True"
     )
 
     header2(f"{REPO_ID}")
@@ -110,7 +111,7 @@ def hf_upload_dirz():
     """
 
     # Folder to upload
-    local_folder_path = "../WX/mywo/ghr1/UL/"  # Path to your local folder
+    local_folder_path = "../WX/mywo/pri1/UL/"  # Path to your local folder
     # Upload to repo root (change to subdir like "folder/" if needed)
     path_in_repo = ""
 
@@ -173,7 +174,7 @@ def hf_delete_spaces():
 
     # Config
     repo_ids = [
-        "Wornu/grh1",
+        "Wornu/pr1",
     ]
     repo_type = "space"
     token = hf_token
