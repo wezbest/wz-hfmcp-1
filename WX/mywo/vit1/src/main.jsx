68cacf96ac5c1240f5700e86af54c36151f4e21c
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-06 02:09:45 +0000

    🫦

--- WX/mywo/vit1/src/main.jsx
+++ WX/mywo/vit1/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="min-h-screen bg-slate-950 ">
+    <div className=" bg-slate-950 p-8">
       <App />
     </div>
   </StrictMode>
