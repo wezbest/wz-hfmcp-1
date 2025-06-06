0cae0a4761114e6d4f386031b698509fd08f30a3
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-06 01:54:39 +0000

    🫦

--- WX/curs/cose1/src/ag1.py
+++ WX/curs/cose1/src/ag1.py
@@ -1,6 +1,8 @@
 # /////////////////////////////////
-# Agentt Calling from curso here
+# Agentt Calling from curso here - This was from the course which you are now abandoning
 # /////////////////////////////////
+
+from smolagents import CodeAgent, InferenceClientModel, WebSearchTool
 
 from .utz import header1
 
@@ -8,4 +10,18 @@ from .utz import header1
 
 
 def ag1_main():
-    header1("Agent Calling from curso")
+    agent_fn1()
+
+# --- Agent Function Test ---
+
+
+def agent_fn1():
+    header1("Agent Function Test - Examples from repository")
+    model = InferenceClientModel(
+        model_id="deepseek-ai/DeepSeek-R1",
+        provider="together",
+    )
+    agent = CodeAgent(tools=[WebSearchTool()],
+                      model=model, stream_outputs=True)
+    agent.run(
+        "How many seconds would it take for a leopard at full speed to run through Pont des Arts?")
