2869030869892b057225b3fd7fa6df3af9cc061e
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-06 02:04:40 +0000

    🫦

--- WX/mywo/vit1/src/main.jsx
+++ WX/mywo/vit1/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import "./App.css"
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <App />
+    </div>
   </StrictMode>
 )
