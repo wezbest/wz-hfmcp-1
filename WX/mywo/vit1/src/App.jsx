181efd9a15f47afd76f595dfba0c47ccb2d375ca
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-06 02:11:26 +0000

    🫦

--- WX/mywo/vit1/src/App.jsx
+++ WX/mywo/vit1/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
+      <div className="">
         <div>
           <a href="https://vite.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
--- WX/mywo/vit1/src/main.jsx
+++ WX/mywo/vit1/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import "./App.css"
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className=" bg-slate-950 p-8">
-      <App />
-    </div>
+    <App />
   </StrictMode>
 )
