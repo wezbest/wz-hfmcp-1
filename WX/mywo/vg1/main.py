8acdaae4c793e53f94ed559ca053e13e8e45c426
wezbest <110793713+wezbest@users.noreply.github.com>
2025-06-06 22:12:18 +0000

    🫦

--- WX/mywo/vg1/main.py
+++ WX/mywo/vg1/main.py
@@ -1,10 +1,20 @@
 # ////////////////////////////////////////////////////////////////
 # Testkng gradio video
 # ////////////////////////////////////////////////////////////////
+import gradio as gr
 
 
-def main():
-    print("Hello from vg1!")
+with gr.Blocks(theme=choose_theme) as demo:
+
+    with gr.Tab("Intro"):
+        intro_tab()
+
+    with gr.Tab("Name Function"):
+        name_func()
+
+
+if __name__ == "__main__":
+    demo.launch()
 
 
 if __name__ == "__main__":
